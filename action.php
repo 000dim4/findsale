871e29811821689c4d711f4b7309b6ce00dab0fb
000dim4 <58269551+000dim4@users.noreply.github.com>
2020-01-18 14:19:00 +0600

    Update action.php

--- action.php
+++ action.php
@@ -35,27 +35,7 @@ echo "<body>";
             );
             echo ('<script type="text/javascript">
             window.onload=function(e){
-                    window.open("https://www.lamoda.ru/catalogsearch/result/?q='.$query.'");
-                    window.open("https://sneakerhead.ru/search/?q='.$query.'");
-                    window.open("https://street-beat.ru/cat/?q='.$query.'");
-                    window.open("https://www.asos.com/ru/search/?q='.$query.'");
-                    window.open("https://www.size.co.uk/search/'.$query.'");
-                    window.open("https://www.endclothing.com/ru/catalogsearch/result/?q='.$query.'");
-                    window.open("https://www.farfetch.com/ru/shopping/search/items.aspx?q='.$query.'");
-                    window.open("https://www.luisaviaroma.com/ru-ru/shop/%D0%BF%D0%BE%D0%B8%D1%81%D0%BA?lvrid=_gm_f&noredirect=1&search='.$query.'");
-                    window.open("https://www.luisaviaroma.com/ru-ru/shop/%D0%B6%D0%B5%D0%BD%D1%89%D0%B8%D0%BD%D1%8B/%D0%BF%D0%BE%D0%B8%D1%81%D0%BA?lvrid=_gw_f&noredirect=1&search='.$query.'");
-                    window.open("https://www.prodirectselect.com/search.aspx?q='.$query.'");
-                    window.open("https://www.footpatrol.com/search/'.$query.'");
-                    window.open("https://www.allikestore.com/default/catalogsearch/result/?q='.$query.'");
-                    window.open("https://www.jdsports.co.uk/search/'.$query.'");
-                    window.open("https://www.basketshop.ru/catalog/search/?s%5Bq%5D='.$query.'");
-                    window.open("https://www.mrporter.com/en-ru/mens/search/'.$query.'");
-                    window.open("https://www.slamdunk.su/catalogue/search?q='.$query.'");
-                    window.open("https://frihet.ru/catalog/search.php?q='.$query.'");
-                    window.open("https://www.sneakersnstuff.com/en/search/searchbytext?key='.$query.'");
-                    window.open("https://www.aphrodite1994.com/search/?q='.$query.'");
-                    window.open("https://www.stuartslondon.com/search/'.$query.'");
-                    window.open("https://www.itkkit.ru/search/?q='.$query.'");
+
             }
             </script>');
 
